5649a91ceecd9d74f733539890272ff853402816
minjee kim <148981647+minjeeki@users.noreply.github.com>
2025-04-22 20:59:52 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,23 +1,10 @@
-choice_num = 6
-
-def back_tracking(idx, cnt):
-    # 종료 조건
-    if (cnt == choice_num):
-        print(*answer)
-        return
-    # 백트래킹 종료 조건
-    if len(lstS) - idx < choice_num - cnt:
-        return
-    # 재귀
-    for i in range(idx, len(lstS)):
-        answer[cnt] = lstS[i]
-        back_tracking(i + 1, cnt + 1)
+from itertools import combinations
 
 while True:
     line = list(map(int, input().split()))
     if line[0] == 0:
         break
     k, *lstS = line
-    answer = [0] * choice_num
-    back_tracking(0, 0)
+    for comb in combinations(lstS, 6):
+        print(*comb)
     print()
