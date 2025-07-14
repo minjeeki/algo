832c9c7538421f66aa6b8898a948a08ed5dab816
minjee kim <148981647+minjeeki@users.noreply.github.com>
2025-07-14 20:43:11 +0900

    [Silver I] Title: IOIOI, Time: 336 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,11 +2,30 @@ N = int(input())
 M = int(input())
 S = input()
 
-PN = 'IO' * N + 'I'
-len_pn = len(PN)
+total = 0
+s_idx = 0
 cnt = 0
-for idx in range(M):
-    if (idx + len_pn) <= M and S[idx] == 'I' \
-        and S[idx + len_pn - 1] == 'I' and S[idx:idx + len_pn] == PN:
-        cnt += 1
-print(cnt)
+
+while s_idx <= M - 2:
+    if S[s_idx] == 'I':
+        e_idx = s_idx
+        cnt = 0
+
+        while e_idx + 2 < M and S[e_idx + 1] == 'O' and S[e_idx + 2] == 'I':
+            cnt += 1
+            e_idx += 2
+
+            if cnt == N:
+                total += 1
+
+                if e_idx + 2 < M and S[e_idx + 1] == 'O' and S[e_idx + 2] == 'I':
+                    s_idx += 2
+                    cnt -= 1
+                else:
+                    break
+        s_idx = e_idx + 1
+
+    else:
+        s_idx += 1
+
+print(total)
